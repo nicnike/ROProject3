f506dd1f9ff875bc7403054c9e58a761549558d4
Docboter <116913789+Docboter@users.noreply.github.com>
2023-05-30 08:28:26 +0200

    comments and unittest created for ML

--- ros_workspace/src/machine_learning/machine_learning/machineLearningNode.py
+++ ros_workspace/src/machine_learning/machine_learning/machineLearningNode.py
@@ -17,8 +17,8 @@ class machineLearning:
         '''!
         Takes radius and shape and makes a prediction about which object it is.
         Returns 1 for unicorn and 0 for cat
-        @param radius
-        @param shape
+        @param radius: Radius of the circle around the object
+        @param shape: Captured shape of the object
         @return 1 for unicorn and 0 for cat
         '''
         returnValue = self.model.predict([[shape, radius]])
@@ -48,8 +48,7 @@ class machineLearningNode(Node):
     def callback_classification(self, msg):
       '''!
       Extracts radius and shape from the message and publishes the classification
-      @param msg
-      @return
+      @param msg: Received message
       '''
       classification = self.ml.prediction(msg.radius, msg.shape)
       classification_msg = MachineLearning()
--- ros_workspace/src/machine_learning/test/test_machineLearning.py
+++ ros_workspace/src/machine_learning/test/test_machineLearning.py
@@ -4,15 +4,24 @@ from ..machine_learning.machineLearningNode import machineLearning, machineLearn
 
 class machineLearningTests(unittest.TestCase):
     def test_ModelInitialization(self):
+        '''!
+        Tests if machine learning model could be loaded.
+        '''
         ml = machineLearning()
         self.assertIsNotNone(ml.model)
 
     def test_PredictFunktion(self):
+        '''!
+        Tests predict function of the machine learning model for correct classification between cat and unicorn.
+        '''
         ml = machineLearning()
         self.assertIsEqual(ml.prediction(radius=140, shape=5), 0)  # Cat
         self.assertIsEqual(ml.prediction(radius=155, shape=8), 1)  # Unicorn
 
     def test_MLNode(self):
+        '''!
+        Tests machine learning node.
+        '''
         node = machineLearningNode()
         node.create_publisher = MagicMock()
         node.get_logger = MagicMock()
